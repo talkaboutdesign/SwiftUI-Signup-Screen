638da5facfe8326893dfce7917ff3846dd60ce8c
Maksim Petriv <talkaboutdesign@gmail.com>
2019-06-14 23:09:53 -0400

    Initial code check in.

--- Buttons/ContentView.swift
+++ Buttons/ContentView.swift
@@ -10,14 +10,54 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Image("swift")
+            .resizable()
+            .frame(width: 80, height: 80)
+            .clipShape(Circle())
+            
+            Text("SwiftUI for Designers")
+            .font(.title)
+            .bold()
+            .foregroundColor(.primary)
+            Text("Start learning SwiftUI Today")
+            .font(.body)
+            .foregroundColor(.secondary)
+            
+            Button(action: {
+                print("Hello")
+            }) {
+                Text("Subscribe Now")
+                .foregroundColor(.white)
+                .padding(10)
+                .frame(width: 200)
+                .cornerRadius(20)
+                .border(Color.blue, width: 1, cornerRadius: 20)
+                .background(Color.blue)
+                .cornerRadius(20)
+                .shadow(radius: 2)
+            }
+            .padding(.top, 20)
+            
+            Text("Copyright 2019, Talkabout Design, LLC")
+            .font(.caption)
+            .foregroundColor(.secondary)
+            .padding(.top, 60)
+        }
+        
     }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .environment(\.colorScheme, .light)
+            ContentView()
+                .environment(\.colorScheme, .dark)
+        }
+       
     }
 }
 #endif
---
+++ README.md
@@ -0,0 +1,3 @@
+#  SwiftUI Buttons
+
+Showcasing how to build out simple UI including a button for Login screen in SwiftUI.
